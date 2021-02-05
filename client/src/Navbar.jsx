4ad682d832e491adb712d45314e6ef048257b97d
Will Burke <billyburke1012@gmail.com>
2021-02-05 17:04:02 -0500

    will PM friday changes feb 5, bootstrap is commented out

--- client/src/Navbar.jsx
+++ client/src/Navbar.jsx
@@ -4,6 +4,9 @@ import {Link} from 'react-router-dom'
 function Navbar(){
     return(
 <nav className="navbar">
+    <div classname="headernav">
+        <h3>Sistema de Alerta Temprana</h3>
+    </div>
     <div className="nav-center">
         <Link to='/'>
             
