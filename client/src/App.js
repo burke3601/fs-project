09f7f7065e3322b7b2c56348f2b87123a366283b
Will Burke <billyburke1012@gmail.com>
2021-02-08 18:53:49 -0500

    minor change on app js, removing typo

--- client/src/App.js
+++ client/src/App.js
@@ -31,7 +31,7 @@ import {REACT_APP_API_KEY} from './config'
 
 
 import Footer from './Footer';
->
+
 const api = {
     
   url: `http://api.openweathermap.org/data/2.5/onecall?lat=14.365316&lon=-91.81191&units=metric&appid=${REACT_APP_API_KEY}`
