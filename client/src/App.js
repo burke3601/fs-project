9c5a8afcbcf6bdcfce328a43bcbd0272c1f6929f
bsummerset <brian.summerset@gmail.com>
2021-02-09 16:07:55 -0500

    updated login and css

--- client/src/App.js
+++ client/src/App.js
@@ -24,7 +24,8 @@ import {
   BrowserRouter as Router, 
   Link,
   Switch,
-  Route
+  Route,
+  Redirect
 } from 'react-router-dom';
 import Sidebar from './Sidebar';
 
@@ -128,6 +129,7 @@ useEffect(()=>{
     <Router>
         {/* <NavbarBootstrap></NavbarBootstrap> */}
         <Route exact path="/">
+
             <Login />
           </Route>
         
