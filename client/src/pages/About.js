10f9849465e948d7a7f5f91bb831cacc42134d1c
Draines1 <rainesdarren@gmail.com>
2021-02-15 15:21:40 -0500

    Updated Readme About

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -2,10 +2,20 @@ import React from "react";
 
 export default function About() {
   return (
-    <section className="section about-section">
-      <h1 className="section-title">about us</h1>
-      <p>
-      Text will go here!
+    <section className="section-about-section">
+      <h1 className="section-title">Sobre Nosotros/About Us</h1>
+      <p className="section-paragraph"> 
+
+      Universidad Galileo, a través del Laboratorio de Mecatrónica, desarrolló el <a href="https://www.galileo.edu/revista-galileo/2019/11/21/un-sistema-que-busca-salvar-vidas/">Sistema de Alerta Temprana Galileo</a>, SATGAL, que tiene como principal objetivo alertar a una población sobre la crecida de un río.
+El sistema consiste en un conjunto de componentes con el objetivo de alertar a la población en casos de peligro, además de recolectar y procesar datos, al ofrecer predicciones temporales sobre un posible comportamiento del evento y posibles efectos. El software, desarrollado específicamente para este proyecto, permite observar en tiempo real la situación de cada cuenca y el comportamiento previo de la misma.
+El proyecto se inició en 2009, posteriormente se estancó, pero en 2016 se reactivó con el objetivo de apoyar a las comunidades alertándolas con anticipación sobre el crecimiento de los niveles de los ríos y, al mismo tiempo, recopilar información para luego crear modelos de predicción.
+La pérdida de vidas humanas, los daños materiales en las casas de las comunidades y crear conciencia de la vulnerabilidad en las poblaciones, son los problemas sociales que atiende el Sistema de Alerta Temprana.
+      </p>
+      <p className="section-paragraph-2">
+Galileo University, through the Mechatronics Laboratory, developed the Galileo Early Warning System, SATGAL, whose main objective is to alert a population about the rising of a river.
+The system consists of a set of components with the aim of alerting the population in cases of danger, in addition to collecting and processing data, by offering temporary predictions about a possible behavior of the event and possible effects. The software, developed specifically for this project, makes it possible to observe in real time the situation of each basin and its previous behavior.
+The project started in 2009, later stalled, but in 2016 it was reactivated with the aim of supporting the communities by alerting them in advance about the increase in river levels and, at the same time, collecting information and then creating prediction models .
+The loss of human life, material damage to the houses of the communities and raising awareness of the vulnerability of the populations are the social problems that the Early Warning System addresses.
       </p>
     </section>
   );
